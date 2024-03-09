2f0053668482d5c2dbab2be380c0cde2afc2eecb
Dmitriy <dmitrijkluchnik47@gmail.com>
2024-03-10 00:15:38 +0300

    finish slider

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,24 @@ const slider = tns({
     slideBy: 'page',
     autoplay: false,
     controls: false,
-    nav: false
+    nav: false,
+    navPosition: "bottom",
+    speed: 1000,
+    responsive: {
+        320: {
+            nav: true,
+        },
+
+        768: {
+            nav: true,
+        },
+        992: {
+            nav: true
+        },
+        1100: {
+            nav: false
+        }
+    }
   });
 
 document.querySelector('.prev').addEventListener ('click', function() {
