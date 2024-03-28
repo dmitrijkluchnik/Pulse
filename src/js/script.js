cb55d211edd666d5d56a1d53cf5fc987668cbb4a
Dmitriy <dmitrijkluchnik47@gmail.com>
2024-03-28 23:46:18 +0300

    finish modal

--- src/js/script.js
+++ src/js/script.js
@@ -67,4 +67,38 @@ $(document).ready(function(){
             $('.overlay, #order').fadeIn('slow');
         });
     });
+    $(window).on('click', function(e){
+        if (e.target.classList.contains('overlay')){
+            $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+        }
+    });
+
+    // validate
+
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+             name: "required",
+             phone: "required",
+             email: {
+                 required: true,
+                 email: true
+             }
+            },
+            messages: {
+             name: "Пожалуйста, введите свое имя",
+             phone: "Пожалуйста, введите свой номер телефона",
+             email: {
+               required: "Пожалуйста, введите свою почту",
+               email: "Неправильно введен адрес почты"
+             }
+           } 
+         });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+375(99) 999-99-99");
 });
